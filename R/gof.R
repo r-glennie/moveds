db9cac1344e7900e50dd25242da7ef306e91d6e4
r-glennie <glennie@glennies.co.uk>
2018-11-22 16:47:17 +0000

    added cvm test

--- R/gof.R
+++ R/gof.R
@@ -1,4 +1,4 @@
-#' Kolmogorov-Smirnov Goodness-of-fit tests   
+#' Cramer-Von-Mises Goodness-of-fit tests   
 #'
 #' @param mod fitted mds model (returned from mds function)
 #' @param delta use a different (dx, dt); otherwise uses mod$ds$delta
@@ -47,18 +47,19 @@ mds.gof <- function(mod, delta = NULL, yquant = 0.95, nint = 1000) {
     x.pdf <- rowSums(pdfm)
     y.pdf <- colSums(pdfm)
     xrange <- seq(0, range[1], length = nint)
-    xgrid <- seq(-ds$aux[3], ds$aux[3] - ds$delta[1], by = ds$delta[1])
+    xgrid <- seq(-ds$aux[3] + 0.5 * ds$delta[1], ds$aux[3] - 0.5 * ds$delta[1], by = ds$delta[1])
     if (length(x.pdf) > length(xgrid)) x.pdf <- x.pdf[-1]
     # perp test
-    gamdat <- data.frame(x = xgrid, y = x.pdf)
+    gamdat <- data.frame(x = xgrid, y = log(x.pdf + 1e-10))
     k <- nrow(gamdat)
     xgam <- gam(y ~ s(x, k = k, fx = TRUE), data = gamdat) 
     x.sp0 <- function(r) {predict(xgam, newdata = data.frame(x = r))}
-    x.sp <- function(x) {pmax(0, x.sp0(x))}
+    x.sp <- function(x) {exp(x.sp0(x))}
     int <- integrate(x.sp, 0, ds$aux[3])$value
-    x.sp <- function(x) {pmax(0, x.sp0(x))/int}
+    x.sp <- function(x) {exp(x.sp0(x))/int}
     x.cdf <- function(x){sapply(x, FUN = function(i){integrate(x.sp, 0, i)$value})}
-    x.ks <- ks.test(abs(ddata[,3]), "x.cdf")
+    library(goftest)
+    x.ks <- cvm.test(abs(ddata[,3]), x.cdf)
     curve(x.cdf, 0, range[1], lwd = 1.5, lty = "dotted", xlab = "Perpendicular Distance", ylab = "CDF")
     plot(ecdf(abs(ddata[,3])), add = T, col = "grey80", lwd = 1.5)
     # forw test
@@ -69,20 +70,20 @@ mds.gof <- function(mod, delta = NULL, yquant = 0.95, nint = 1000) {
     yrange <- yrange[yrange <= range[2]]
     y.pdf <- y.pdf[ygrid <= range[2]]
     ygrid <- ygrid[ygrid <= range[2]]
-    gamdat <- data.frame(x = ygrid, y = y.pdf)
+    gamdat <- data.frame(x = ygrid, y = log(y.pdf + 1e-10))
     k <- nrow(gamdat)
     ygam <- gam(y ~ s(x, k = k, fx = TRUE), data = gamdat) 
     y.sp0 <- function(r) {predict(ygam, newdata = data.frame(x = r))}
-    y.sp <- function(y) {pmax(0, y.sp0(y))}
+    y.sp <- function(y) {exp(y.sp0(y))}
     y.spy <- y.sp(yrange)
     int <- integrate(y.sp, 0, range[2])$value
-    y.sp <- function(y) {pmax(0, y.sp0(y))/int}
+    y.sp <- function(y) {exp(y.sp0(y))/int}
     y.cdf <- function(x){sapply(x, FUN = function(i){integrate(y.sp, 0, i)$value})}
     ydat <- ddata[,4]
-    y.ks <- ks.test(ydat, "y.cdf")
+    y.ks <- cvm.test(ydat, y.cdf)
     curve(y.cdf, 0, range[2], lwd = 1.5, lty="dotted", xlab = "Forward Distance", ylab = "CDF")
     plot(ecdf(ddata[,4]), add = T, col = "grey80", lwd = 1.5)
-    cat("Kolomgorov-Smirnov Test\n\n")
+    cat("Cramer-Von-Mises Test\n\n")
     cat("Perpendicular PDF: p-value = ", x.ks$p.value, " D = ", x.ks$statistic, "\n")
     cat("Forward PDF: p-value = ", y.ks$p.value, " D = ", y.ks$statistic, "\n\n")
     return(list(x.ks = x.ks, y.ks = y.ks))
@@ -96,21 +97,21 @@ mds.gof <- function(mod, delta = NULL, yquant = 0.95, nint = 1000) {
     x.vals <- rep(xgrid, length(ygrid))
     y.vals <- rep(ygrid, each = length(xgrid))
     r.vals <- sqrt(x.vals^2 + y.vals^2)
-    gamdat <- data.frame(x = r.vals, y = r.vals * as.vector(pdfm))
+    gamdat <- data.frame(x = r.vals, y = log(r.vals)  + as.vector(log(pdfm)))
     k <- length(xgrid)
     fitgam <- gam(y ~ s(x, k = k, fx=TRUE), data = gamdat) 
     r.sp0 <- function(r) {predict(fitgam, newdata = data.frame(x = r))}
-    r.sp <- function(r) {pmax(0, r.sp0(r))}
+    r.sp <- function(r) {exp(r.sp0(r))}
     rrange <- seq(0,ds$aux[3])
     r.spy <- r.sp(rrange) 
     int <- integrate(r.sp, 0, ds$aux[3])$value 
-    r.sp <- function(r) {pmax(0, r.sp0(r)) / int}
+    r.sp <- function(r) {exp(r.sp0(r)) / int}
     r.cdf <- function(x){sapply(x, FUN = function(i){integrate(r.sp, 0, i)$value})}
     rdat <- sqrt(ddata[,3]^2 + ddata[,4]^2)
-    r.ks <- ks.test(rdat, "r.cdf")
+    r.ks <- cvm.test(rdat, r.cdf)
     curve(r.cdf, 0, ds$aux[3], lwd = 1.5, lty="dotted", xlab = "Radial Distance", ylab = "CDF")
     plot(ecdf(rdat), add = T, col = "grey80", lwd = 1.5)
-    cat("Kolomgorov-Smirnov Test\n\n")
+    cat("Cramer-Von-Mises Test\n\n")
     cat("Radial PDF: p-value = ", r.ks$p.value, " D = ", r.ks$statistic, "\n")
     return(list(r.ks = r.ks))
   }
